e7754d634a5a4f026df2dd014a607bef7d371f2e
PC\PCUser <satoraitokun1995@gmail.com>
2017-02-04 15:04:23 +0900

    no message

---
+++ Assets/attack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attack : MonoBehaviour {
+
+    public Transform player;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.LookAt(player.transform.position);
+	
+	}
+}
---
+++ Assets/skyenemy.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class skyenemy : MonoBehaviour {
+
+    bool on = false;
+    public GameObject empty;
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+ //       if (on)
+//        {
+//             attack(empty.transform.position);
+//        
+//        }
+	
+	}
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.tag == "player")
+        {
+            Instantiate(empty, player.transform.position, player.transform.rotation);
+            //on = true;
+        }
+
+       
+    }
+
+
+    void attack(Vector3 position)
+    {
+
+        transform.LookAt(position);
+
+        transform.Translate(Vector3.forward * 0.4f);
+       
+    }
+
+}
